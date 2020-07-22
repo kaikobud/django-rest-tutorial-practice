4b78bacf34fc8848e16d82e689e17b9112df91ba
kaikobud <kaikobudsarker@gmail.com>
2020-07-22 20:41:56 +0600

    class based api view

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,8 +1,11 @@
 # Created by kaikobud at ২১/৭/২০
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 urlpatterns = [
-    path('snippet/', views.snippet_list),
-    path('snippet/<int:pk>', views.snippet_details),
+    path('snippet/', views.SnippetList.as_view()),
+    path('snippet/<int:pk>', views.SnippetDetail.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
